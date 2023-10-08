e933c580556a1225761dd9b574c7355f9f46c87d
Nilesh Sharma <sharmanilesh7105@gmail.com>
2023-10-08 20:28:26 +0530

    added some files

--- question4.py
+++ question4.py
@@ -1,4 +1,10 @@
 m=int(input("Enter: "))
 n=int(input("Enter: "))
-while True:
-        
+a=[]
+for i in range(n):
+    for j in range(m):
+        n=int(input("Enter the value: "))
+        a.append(n)
+for i in a:
+    if m==n:
+        b=[a[m*(i)]]
